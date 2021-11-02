594777eea903d4014dc3bfd8087e0d18c35a76cb
satishkumarm7 <satishkumarm@gmail.com>
2021-11-03 00:07:55 +0530

    renamed the button mapping the audio java class for audio loop

--- app/src/main/java/com/skel/sound/MainActivity.java
+++ app/src/main/java/com/skel/sound/MainActivity.java
@@ -1,6 +1,7 @@
 package com.skel.sound;
 
 import android.content.Context;
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
@@ -24,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
     private Button sbButton12;
     private MediaPlayer mp = new MediaPlayer();
 
+
+
     private View.OnClickListener respondToClick = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
@@ -33,57 +36,57 @@ public class MainActivity extends AppCompatActivity {
 
             switch (v.getId()) {
                 case R.id.sb_btn1:
-                    mp = MediaPlayer.create(context, R.raw.explosion);
+                    mp = MediaPlayer.create(context, R.raw.fouragain);
                     mp.setVolume(100,100);
                     mp.start();
 
                     break;
                 case R.id.sb_btn2:
-                    mp = MediaPlayer.create(context, R.raw.name);
+                    mp = MediaPlayer.create(context, R.raw.bowledhim);
                     mp.start();
 
                     break;
-                case R.id.sb_btn3:
-                    mp = MediaPlayer.create(context, R.raw.eugh1);
-                    mp.start();
-                    break;
+               // case R.id.sb_btn3:
+                 //   mp = MediaPlayer.create(context, R.raw.greatshot);
+                   // mp.start();
+                   // break;
                 case R.id.sb_btn4:
-                    mp = MediaPlayer.create(context, R.raw.plosion);
+                    mp = MediaPlayer.create(context, R.raw.thatisawide2);
                     mp.start();
                     break;
                 case R.id.sb_btn5:
-                    mp = MediaPlayer.create(context, R.raw.pull);
+                    mp = MediaPlayer.create(context, R.raw.thatsagoodshot);
                     mp.start();
                     break;
 
-                case R.id.sb_btn6:
-                    mp = MediaPlayer.create(context, R.raw.eugh2);
-                    mp.start();
-                    break;
+               // case R.id.sb_btn6:
+               //     mp = MediaPlayer.create(context, R.raw.thatsasix2);
+               //     mp.start();
+              //      break;
                 case R.id.sb_btn7:
-                    mp = MediaPlayer.create(context, R.raw.sion);
+                    mp = MediaPlayer.create(context, R.raw.whatasix);
                     mp.start();
                     break;
-                case R.id.sb_btn8:
-                    mp = MediaPlayer.create(context, R.raw.itai);
-                    mp.start();
-                    break;
-                case R.id.sb_btn9:
-                    mp = MediaPlayer.create(context, R.raw.eugh3);
-                    mp.start();
-                    break;
+               // case R.id.sb_btn8:
+                 //   mp = MediaPlayer.create(context, R.raw.stadium);
+                  //  mp.start();
+                   // break;
+              //  case R.id.sb_btn9:
+               //     mp = MediaPlayer.create(context, R.raw.eugh3);
+               //     mp.start();
+               //     break;
                 case R.id.sb_btn10:
-                    mp = MediaPlayer.create(context, R.raw.n);
+                    mp = MediaPlayer.create(context, R.raw.greatshot);
                     mp.start();
                     break;
                 case R.id.sb_btn11:
-                    mp = MediaPlayer.create(context, R.raw.yamero);
+                    mp = MediaPlayer.create(context, R.raw.thatsasix2);
                     mp.start();
                     break;
-                case R.id.sb_btn12:
-                    mp = MediaPlayer.create(context, R.raw.eugh4);
-                    mp.start();
-                    break;
+               // case R.id.sb_btn12:
+               //     mp = MediaPlayer.create(context, R.raw.eugh4);
+                //    mp.start();
+               //     break;
             }
         }
     };
@@ -93,31 +96,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //AppRater.app_launched(this);
-        sbButton1 = (Button)findViewById(R.id.sb_btn1);
-        sbButton2 = (Button)findViewById(R.id.sb_btn2);
-        sbButton3 = (Button)findViewById(R.id.sb_btn3);
-        sbButton4 = (Button)findViewById(R.id.sb_btn4);
-        sbButton5 = (Button)findViewById(R.id.sb_btn5);
-        sbButton6 = (Button)findViewById(R.id.sb_btn6);
-        sbButton7 = (Button)findViewById(R.id.sb_btn7);
-        sbButton8 = (Button)findViewById(R.id.sb_btn8);
-        sbButton9 = (Button)findViewById(R.id.sb_btn9);
-        sbButton10 = (Button)findViewById(R.id.sb_btn10);
-        sbButton11= (Button)findViewById(R.id.sb_btn11);
-        sbButton12 = (Button)findViewById(R.id.sb_btn12);
+        sbButton1 = (Button) findViewById(R.id.sb_btn1);
+        sbButton2 = (Button) findViewById(R.id.sb_btn2);
+        // sbButton3 = (Button)findViewById(R.id.sb_btn3);
+        sbButton4 = (Button) findViewById(R.id.sb_btn4);
+        sbButton5 = (Button) findViewById(R.id.sb_btn5);
+        // sbButton6 = (Button)findViewById(R.id.sb_btn6);
+        sbButton7 = (Button) findViewById(R.id.sb_btn7);
+       // sbButton8 = (Button) findViewById(R.id.sb_btn8);
+        //  sbButton9 = (Button)findViewById(R.id.sb_btn9);
+        sbButton10 = (Button) findViewById(R.id.sb_btn10);
+        sbButton11 = (Button) findViewById(R.id.sb_btn11);
+        // sbButton12 = (Button)findViewById(R.id.sb_btn12);
 
         sbButton1.setOnClickListener(respondToClick);
         sbButton2.setOnClickListener(respondToClick);
-        sbButton3.setOnClickListener(respondToClick);
+        // sbButton3.setOnClickListener(respondToClick);
         sbButton4.setOnClickListener(respondToClick);
         sbButton5.setOnClickListener(respondToClick);
-        sbButton6.setOnClickListener(respondToClick);
+        //  sbButton6.setOnClickListener(respondToClick);
         sbButton7.setOnClickListener(respondToClick);
-        sbButton8.setOnClickListener(respondToClick);
-        sbButton9.setOnClickListener(respondToClick);
+       // sbButton8.setOnClickListener(respondToClick);
+        //  sbButton9.setOnClickListener(respondToClick);
         sbButton10.setOnClickListener(respondToClick);
         sbButton11.setOnClickListener(respondToClick);
-        sbButton12.setOnClickListener(respondToClick);
+        // sbButton12.setOnClickListener(respondToClick);
     }
+        public void PlayBackgroundSound (View view){
+            Intent intent = new Intent(MainActivity.this, BackgroundSoundService.class);
+            startService(intent);
+        }
+
 
 }
